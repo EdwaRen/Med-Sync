79488a6cd097a842d2099e2a5f2a8e8c0cce03e1
Edward Ren <eddie.ren.2013@gmail.com>
2017-03-20 22:41:35 -0400

    Started adding Model View Controller elements

---
+++ Med-Sync/MedSync4.0/MedSync4.0/SignInVC.swift
@@ -0,0 +1,47 @@
+//
+//  SignInVC.swift
+//  MedSync4.0
+//
+//  Created by - on 2017/03/20.
+//  Copyright © 2017 Danth. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
