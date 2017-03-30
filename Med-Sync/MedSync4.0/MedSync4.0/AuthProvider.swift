017c7c6c182e5e185c01575d5fb7496216b4b592
Edward Ren <eddie.ren.2013@gmail.com>
2017-03-30 00:55:53 -0400

    Created new classes to increase modularity

--- Med-Sync/MedSync4.0/MedSync4.0/AuthProvider.swift
+++ Med-Sync/MedSync4.0/MedSync4.0/AuthProvider.swift
@@ -8,7 +8,71 @@
 
 import Foundation
 import FirebaseAuth
+typealias LoginHandler = (_ msg:String?) -> Void;
+
+struct LoginErrorCode {
+    static let INVALID_EMAIL = "Invalid email address, please provide another";
+    static let WRONG_PASSWORD = "Wrong password, please enter the correct password";
+    static let PROBLEM_CONNECTING = "Problem connecting to databse";
+    static let USER_NOT_FOUND = "User not found, please register";
+    static let EMAIL_ALREADY_IN_USE = "Email already in use, please use another email";
+    static let WEAK_PASSWORD = "Password should be at least 6 characters long";
+    
+}
 
 class AuthProvider {
+    private static let _instance = AuthProvider();
+    static var Instance: AuthProvider {
+        return _instance;
+    }
+    
+    func login(withEmail: String, password: String, loginHandler: LoginHandler?) {
+        FIRAuth.auth()?.signIn(withEmail: withEmail, password: password, completion: { (user, error) in
+            
+            if error != nil {
+                self.handlerError(err: error as! NSError, loginHandler: loginHandler);
+            } else {
+                loginHandler?(nil);
+            }
+            
+            
+            
+        })
+    } // login func
+    
+    private func handlerError(err: NSError, loginHandler: LoginHandler?) {
+        
+        if let errCode = FIRAuthErrorCode(rawValue: err.code) {
+            
+            switch errCode {
+            case .errorCodeWrongPassword:
+                loginHandler!(LoginErrorCode.WRONG_PASSWORD);
+                break;
+                
+            case .errorCodeInvalidEmail:
+                loginHandler!(LoginErrorCode.INVALID_EMAIL);
+                break;
+                
+            case .errorCodeUserNotFound:
+                loginHandler!(LoginErrorCode.USER_NOT_FOUND);
+                break;
+                
+            case .errorCodeEmailAlreadyInUse:
+                loginHandler!(LoginErrorCode.EMAIL_ALREADY_IN_USE);
+                break;
+                
+            case .errorCodeWeakPassword:
+                loginHandler!(LoginErrorCode.WEAK_PASSWORD);
+                break;
+            default:
+                loginHandler!(LoginErrorCode.PROBLEM_CONNECTING);
+
+                break;
+                
+            }
+            
+        }
+        
+    }
     
 }
--- Med-Sync/MedSync4.0/MedSync4.0/SignInVC.swift
+++ Med-Sync/MedSync4.0/MedSync4.0/SignInVC.swift
@@ -18,8 +18,7 @@ class SignInVC: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+               // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +27,20 @@ class SignInVC: UIViewController {
     }
     @IBAction func login(_ sender: Any) {
         //performSegue(withIdentifier: CONTACT_SEGUE, sender: nil)
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            AuthProvider.Instance.login(withEmail: emailTextField.text!, password: passwordTextField.text!, loginHandler: { (message) in
+                
+                if message != nil {
+                    self.alertTheUser(title: "Problem with authentication", message: message!)
+                } else {
+                    print("LOGIN COMPLETE")
+                }
+                
+            })
+        }
+        
+        /*
         if emailTextField.text != "" && passwordTextField.text != "" {
             FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
                 if error != nil {
@@ -46,10 +59,18 @@ class SignInVC: UIViewController {
                 }
             })
         }
+ */
         
     }
     
     @IBAction func signUp(_ sender: Any) {
+    }
+    
+    private func alertTheUser(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert);
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil);
+        alert.addAction(ok);
+        present(alert, animated: true, completion: nil);
     }
 
 
